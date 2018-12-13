bb526e97d3303f07aaa0a0f1160323e039249d57
Tabrez Basha <tab22rez@gmail.com>
2018-12-13 16:01:52 +0530

    Webpack and babel config for local imports in dev

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['es2015', 'react', 'stage-0']
           }
         }
       },
