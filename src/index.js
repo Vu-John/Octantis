8eb0d77c25e43cfa7dad0c26fe5fd7bd5077cfbd
Tabrez Basha <tab22rez@gmail.com>
2019-03-29 10:17:48 +0530

    Export paginator to use it independently

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ export { default as SimpleResourceList } from './simpleresourselist/index.jsx';
 export { default as DataTable } from './datatable/table/index.jsx';
 export { default as TopNav } from './topnav/index.jsx';
 export { default as Icon } from './icon/index.jsx';
+export { default as Paginator } from './paginator/index.jsx';
+
 
